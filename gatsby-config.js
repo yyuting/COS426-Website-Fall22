a55fd31bb9c2358a899e48a19b63eab816f3603f
Yuting Yang <yuting@Yutings-MBP-2.fios-router.home>
2022-08-30 16:57:11 -0400

    update course information for fall 2022

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,12 @@ const numLecturesPosted = 22; // Increment every time a lecture is posted
 const numPreceptsPosted = 12; // Increment every time a precept is posted
 // Annual Settings
 const semester = 2022; // Increment every year
-const firstDayOfSemester = '01-24'; // Adjust based off of academic calendar
+const firstDayOfSemester = '09-06'; // Adjust based off of academic calendar
 const firstLecture = 'Tuesday'; // Change based off of Registrar scheduling
 const secondLecture = 'Thursday'; // Change based off of Registrar scheduling
-const piazzaURL = 'https://piazza.com/princeton/spring2022/cos426/home'; // Piazza Link
+const piazzaURL = 'https://princeton.instructure.com/courses/7766/external_tools/889?display=borderless'; // Piazza Link
 // NB: INCLUDE a leading slash; EXCLUDE a trailing slash
-const pathURL = '/courses/archive/spring22/cos426'; // Subfolder location; necessary for internal links.
+const pathURL = '/courses/archive/fall22/cos426'; // Subfolder location; necessary for internal links.
 const baseURL = 'https://www.cs.princeton.edu'; // Root host domain for site
 
 module.exports = {
@@ -26,14 +26,14 @@ module.exports = {
         },
         title: `COS 426: Computer Graphics (${semester}) | Princeton University`,
         siteUrl: `${baseURL}${pathURL}`,
-        description: `The Princeton University course website for the Spring ${semester} term of COS 426: Computer Graphics`,
+        description: `The Princeton University course website for the Fall ${semester} term of COS 426: Computer Graphics`,
         author: 'ReillyBova',
         keywords: [
             'COS 426',
             'Princeton',
             'Graphics',
             'Computer Science',
-            `Spring ${semester}`,
+            `Fall ${semester}`,
         ],
         relatedLinks: {
             university: 'princeton.edu',
